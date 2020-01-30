c2fd3331e40a421771ffbb22d5f4ba8353c1503c
AlexisBenamar <alexis.benamar@viacesi.fr>
2020-01-30 15:11:45 +0100

    improved dev conditions

--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,24 @@
 import Discord from 'discord.js'
 import dotenv from 'dotenv'
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config()
-} 
+const isDev = process.env.NODE_ENV !== 'production'
+
+if (isDev) {
+  dotenv.config() 
+}
 
 const bot = new Discord.Client()
 
 bot.on('ready', () => {
+  bot.user.setStatus(isDev ? 'invisible' : 'online')
   console.log('👋 henlo')
 })
 
 bot.on('message', msg => {
   let { content, author, channel } = msg
+
+  //when testing, ignores all but test serv messages
+  if (isDev && msg.guild.name !== process.env.TEST_SERV) return
   
   //henlo
   if (RegExp(/\bvnrbot\b/i).test(content) || msg.isMemberMentioned(bot.user)) {
